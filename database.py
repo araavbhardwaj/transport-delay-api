1c4bab4f37b349b295f48b8bf6e88f9fba4e701d
Araav Bhardwaj <araavbhardwaj@gmail.com>
2026-03-20 12:44:27 +0000

    Added database models

--- database.py
+++ database.py
@@ -5,4 +5,5 @@ DATABASE_URL = "sqlite:///./transport.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
+
 Base = declarative_base()
